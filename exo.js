a7526aaeb6ece898548f73a66e1f7dc0c132a2d2
224rifche <cherif2862@gmail.com>
2025-11-03 08:01:12 +0100

    first commit

---
+++ exo.js
@@ -0,0 +1,31 @@
+/*console.log("**** Les premiers variables *****");
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log("My dog's name is " + petDog );
+console.log("My cat's name is " + petCat);
+
+console.log("**** Les deuxiemes variables *****");
+var catSound = "purr";
+var dogSound = "woof";
+console.log(petCat + " says " + catSound);
+var catSound = "meow";
+console.log(petCat + " now says " + catSound);*/
+
+//var score = 15;
+//console.log("Mid-level skill:" + (score > 0 && score < 10));
+
+/*var TimeRemaining = 0;
+var energy = 10;
+
+console.log("Game over: " + (TimeRemaining == 0 || energy == 0));*/
+
+var num1 = 2;
+var num2 = 5;
+
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var result1 = (test1 == 0);
+var result2 = (test2 == 0);
+
+console.log("is " + num1 + " an even number: " + result1);
+console.log("is " + num2 + " an even number: " + result2);
